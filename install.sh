95fd68d194684883624263039f2281e88d80f94d
Farhan Aziz <121700769+Frhaanaziz@users.noreply.github.com>
2024-01-31 04:54:30 +0000

    Update zshrc function in install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ zshrc() {
 # sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
 # sudo dpkg-reconfigure --frontend noninteractive tzdata
 
-zshrc
+zshrc 
 
 # make directly highlighting readable - needs to be after zshrc line
 echo "" >> ~/.zshrc
